b2f872e7f31744ebd5df5c88ebddb90a96494770
tercerolopezalexisuriel55 <83981213+tercerolopezalexisuriel55@users.noreply.github.com>
2021-06-05 18:16:24 -0500

    version propia
    Se agregaron los cambios funcionales para concordar con la notacion del libro de Burden

--- PLagrange.py
+++ PLagrange.py
@@ -1,39 +1,62 @@
-# Interpolacion de Lagrange
-# divisoresL solo para mostrar valores
+#Modulo: PLagrange.py import import PLagrange as PL
+#Implementacion del polinomio interpolante (de grado n) 
+#de lagrange para n+1 puntos.
+#Autor: Alexis Tercero (@math.by.uri) 
+#Exitoso
 import numpy as np
 import sympy as sym
-import matplotlib.pyplot as plt
-from math import  *
 
-def PLagrange(xi,fi,fun):
+def Lnk(n,k,xi,x, show=False):
+  ''' Termino de Lagrange
+  L_{n,k}(x)=\Pi_{i=0 \\ i\neq k}^n \frac{(x-x_i)}{(x_k-x_i)}
+  n: int: numero de puntos
+  k: int: punto de refereocia
+  xi: list: conjunto de puntos muestra
+  show=False: Boolean: muestra o no datos detallados del proceso'''
+  numerador = 1
+  denominador = 1
+  for i  in range(n):
+    if (i!=k):
+      numerador = numerador*(x-xi[i])
+      denominador = denominador*(xi[k]-xi[i])
+  if show:
+    print('L_{}'.format(k),'=')
+    print(numerador)
+    print('-'*35)
+    print(denominador,'\n')
+  return numerador/denominador
+  
+def PLagrange(xi,fi,show=False):
+  '''Polinomio de lagrange
+  Parametros------------------------------------------------------
+  P(x)=\sum_{k=0}^n f(x_k)L_{n,k}(x)
+  xi: list: conjunto de puntos muestra
+  fi: list: valores mapeados de f en xi
+  show=False: Boolean: muestra o no datos detallados del proceso
+  ----------------------------------------------------------------
+  Retorna una funcion lambda Polinomio P(x)
+  '''
+  print('valores de fi: ',fi)
+  print('valores de xi: ',xi)
   # PROCEDIMIENTO
   # Polinomio de Lagrange
-  n = len(xi)
-  x = sym.Symbol('x')
+  n = len(xi)# numero de puntos 
+  x = sym.Symbol('x') #expresamos la variable 
   polinomio = 0
-  divisorL = np.zeros(n, dtype = float)
-  for i in range(0,n,1):
-      # Termino de Lagrange
-      numerador = 1
-      denominador = 1
-      for j  in range(0,n,1):
-          if (j!=i):
-              numerador = numerador*(x-xi[j])
-              denominador = denominador*(xi[i]-xi[j])
-      terminoLi = numerador/denominador
 
-      polinomio = polinomio + terminoLi*fi[i]
-      divisorL[i] = denominador
+  for k in range(n):#generacion del polinomio interpolante
+    terminoLk = Lnk(n,k,xi,x,show)
+    polinomio = polinomio + terminoLk*fi[k]
 
   # simplifica el polinomio
   polisimple = polinomio.expand()
 
   # SALIDA
-  print('    valores de fi: ',fi)
-  print('divisores en L(i): ',divisorL)
-  print()
-  print('Polinomio de Lagrange, expresiones')
-  print(polinomio)
+  #print('divisores en L(i): ',divisorL)
+  if show:
+    print()
+    print('Polinomio de Lagrange, expresiones')
+    print(polinomio)
   print()
   print('Polinomio de Lagrange: ')
   print(polisimple)
